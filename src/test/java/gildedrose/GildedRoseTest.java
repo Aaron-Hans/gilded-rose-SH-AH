b3b69f78c674e4df3696d0fe359cde5ae0cf6996
Najib <btlknajib@gmail.com>
2022-12-18 15:58:19 +0100

    Adding Test with All extended Item & should build

--- src/test/java/gildedrose/GildedRoseTest.java
+++ src/test/java/gildedrose/GildedRoseTest.java
@@ -1,15 +1,48 @@
 package gildedrose;
 
-import gildedrose.Shop;
-
 import org.junit.jupiter.api.Test;
 
+import gildedrose.items.AgedItem;
+import gildedrose.items.ConjuredItem;
+import gildedrose.items.LegendaryItem;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import java.util.ArrayList;
+import java.util.List;
 
 class GildedRoseTest {
     @Test
     void should_build() {
-        Shop shop = new Shop();
+    	List<Item> itemsList = new ArrayList<Item>();
+        Shop shop = new Shop(itemsList);
         assertEquals(shop.getClass(), Shop.class);
+    }
+    
+    @Test
+    void agingItemMustUpgradeQuality() {
+    	Item agedBrie = new AgedItem("Aged Brie", 10, 10);
+    	Item secondAgedBrie = new AgedItem("Aged Brie", 10, 10);
+    	
+    	secondAgedBrie.updateItem();
+    	assertNotEquals(agedBrie, secondAgedBrie);
+    }
+    
+    @Test
+    void conjuredItemMustUpgradeQuality() {
+    	Item conjuredItem = new ConjuredItem("Conjured Brie", 10, 10);
+    	Item secondConjuredItem = new ConjuredItem("Conjured Brie", 10, 10);
+    	
+    	secondConjuredItem.updateItem();
+    	assertNotEquals(conjuredItem, secondConjuredItem);
+    }
+    
+    @Test
+    void LegendaryItemMustNotChange() {
+    	Item legendaryItem = new LegendaryItem("Legendary Brie", 10, 10);
+    	
+    	legendaryItem.updateItem();
+    	assertEquals(legendaryItem, legendaryItem);
     }
 }
